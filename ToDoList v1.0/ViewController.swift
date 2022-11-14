e717cf333121fd7db1b2cde7673f7da461034a28
Isa199510 <isa.arbuhov@mail.ru>
2022-11-14 11:38:15 +0600

    Initial Commit

---
+++ ToDoList v1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList v1.0
+//
+//  Created by Иса on 14.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
